bab97ab8b4e1d8607c30ebfc6d40a4919512a3dd
Marc Sevilleja <marc.sevilleja@gmail.com>
2024-02-13 22:34:27 -0800

    Implemented most swordmaster anims and added an anim helper class

---
+++ Assets/Scripts/PlayerAnimStates.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerAnimStates
+{
+    public static string IDLE { get { return "Idle"; } }
+    public static string RUN { get { return "Run"; } }
+    public static string JUMP { get { return "Jump"; } }
+    public static string JUMPTOFALL { get { return "JumpToFall"; } }
+    public static string FALL { get { return "Fall"; } }
+    public static string LAND { get { return "Land"; } }
+    public static string DODGE { get { return "Dodge"; } }
+    public static string ATTACK_1 { get { return "Attack_1"; } }
+    public static string ATTACK_2 { get { return "Attack_2"; } }
+    public static string HURT { get { return "Hurt"; } }
+    public static string DEATH { get { return "Death"; } }
+    
+}
